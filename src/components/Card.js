758436e714fe5f7fb69470fd6c9d352a293e2ca5
Daria Ziobrowska <darianocon@gmail.com>
2023-02-28 19:47:58 +0100

    Add some new classes to style

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,16 +3,17 @@ import React from 'react';
 function Card(props) {
   return (
     <div className='card'>
-      <img src={`./${props.img}`} alt={props.alt} className='card-img'/>
+      <img src={`../../images/${props.img}`} alt={props.alt} className='card-img'/>
       <div className='card-stats'>
         <img src='../images/star.png' alt='star' className='card-star'></img>
           <span>{props.rating}</span>
           <span className='gray'>({props.reviewCount}) •</span>
-          <span className='gray'>{props.country}</span>
+          <span className='gray'>{props.location}</span>
         </div>
-        <p>{props.title}</p>
-        <p><span className='bold'>From ${props.price}</span> / person</p>
+        <p className='card-title'>{props.title}</p>
+        <p className='card-price'><span className='bold'>From ${props.price}</span> / person</p>
     </div>
+
   )
 }
 
