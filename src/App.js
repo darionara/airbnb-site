7be024c775391af7a7efe21affe366b5740e89f8
Daria Ziobrowska <darianocon@gmail.com>
2023-02-28 12:03:03 +0100

    Create props in Card component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,13 @@ function App() {
     <div className="App">
       <Navbar />
       <Hero />
-      <Card />
+      <Card img='katie-zaferes.png'
+            alt='Katie Zaferes photo'
+            rating='5.0'
+            reviewCount={6}
+            country='USA'
+            title='Life lessons with Katie Zaferes'
+            price={136} />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-function Card() {
+function Card(props) {
   return (
     <div className='card'>
-      <img src="./katie-zaferes.png" alt="Katie Zafires" className='card-img'/>
+      <img src={`./${props.img}`} alt={props.alt} className='card-img'/>
       <div className='card-stats'>
         <img src='./star.png' alt='star' className='card-star'></img>
-          <span>5.0</span>
-          <span className='gray'>(6) •</span>
-          <span className='gray'>USA</span>
+          <span>{props.rating}</span>
+          <span className='gray'>({props.reviewCount}) •</span>
+          <span className='gray'>{props.country}</span>
         </div>
-        <p>Life lessons with Katie Zaferes</p>
-        <p><span className='bold'>From $136</span> / person</p>
+        <p>{props.title}</p>
+        <p><span className='bold'>From ${props.price}</span> / person</p>
     </div>
   )
 }
