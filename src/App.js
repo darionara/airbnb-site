ef89297d5ec1b52c741df813b5f458578633f9f5
Daria Ziobrowska <darianocon@gmail.com>
2023-02-28 19:49:24 +0100

    Import data and use map to display a single Card

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
+import data from './data/data'
 
 function App() {
+  
+  const cardItems = data.map(cardItem => {
+    return <Card key={cardItem.id}
+                 img={cardItem.coverImg}
+                 alt={cardItem.alt}
+                 rating={cardItem.stats.rating}
+                 reviewCount={cardItem.stats.reviewCount} 
+                 location={cardItem.location}
+                 title={cardItem.title}
+                 price={cardItem.price}
+                />
+  });
+  
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card img='../images/katie-zaferes.png'
-            alt='Katie Zaferes photo'
-            rating='5.0'
-            reviewCount={6}
-            country='USA'
-            title='Life lessons with Katie Zaferes'
-            price={136} />
+      <section className='cards-list'>
+        {cardItems}
+      </section>
     </div>
   );
 }
